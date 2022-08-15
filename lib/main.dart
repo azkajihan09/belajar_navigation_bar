96aaa2d757981cec68219f7597361b9400984f80
Windy Sabtami <windysabtami92@gmail.com>
2022-08-15 18:34:20 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Widget028(),
+    );
+  }
+}
+
+class Widget028 extends StatefulWidget {
+  @override
+  State<Widget028> createState() => _Widget028State();
+}
+
+class _Widget028State extends State<Widget028> {
+  int _currentIndex = 0;
+  List<Widget> body = [
+    Icon(Icons.home),
+    Icon(Icons.menu),
+    Icon(Icons.person),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget028'),
+      ),
+      body: Center(
+        child: body[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int newIndex) {
+          setState(() {
+            _currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Menu',
+            icon: Icon(Icons.menu),
+          ),
+          BottomNavigationBarItem(
+            label: 'Person',
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+    );
+  }
+}
